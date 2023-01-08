edc569cec6e4c64b68a22af784d010213c824cec
Aladdin <SirAnxiousALot@mail.com>
2023-01-08 23:38:46 +0200

    Time: 0 ms (100.00%), Space: 6 MB (57.26%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int fib(int n) {
+        if(n == 0)
+            return 0;
+        else if(n == 1)
+            return 1;
+        else
+        {
+            int first = 0;
+            int second = 1;
+            int next = 0;
+            for(int i =0; i<n-1; i++)
+            {
+                next = first+second;
+                first = second;
+                second = next;
+            }
+            return next;
+        }
+        
+        
+    }
+};
