a7f4d19c402946367130072f00ba0c7543da167f
Aladdin <SirAnxiousALot@mail.com>
2023-01-08 23:35:48 +0200

    Time: 16 ms (87.04%), Space: 13.5 MB (13.30%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,5 +1,9 @@
-class Solution:
-    def fib(self, n: int) -> int:
+class Solution(object):
+    def fib(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
         if n == 0:
             return 0
         if n == 1:
@@ -12,6 +16,3 @@ class Solution:
             first = second
             second = new
         return new
-            
-            
-        
