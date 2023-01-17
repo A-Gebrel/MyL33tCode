cb24fb470b529f411fa08cb4dd29e8d3557b587d
Aladdin <SirAnxiousALot@mail.com>
2023-01-17 22:39:51 +0200

    Time: 4 ms (36.89%), Space: 6.4 MB (11.59%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isValid(string s) 
+    {
+        std::stack<char> stk;
+        for (char c : s) {
+            if (c == '(' || c == '{' || c == '[') {
+                stk.push(c);
+            } else {
+                if (stk.empty()) return false;
+                if (c == ')' && stk.top() != '(') return false;
+                if (c == '}' && stk.top() != '{') return false;
+                if (c == ']' && stk.top() != '[') return false;
+                stk.pop();
+            }
+        }
+        return stk.empty();
+    }
+};
