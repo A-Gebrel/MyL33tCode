b8973159d7a9bc5ce083537a8d3a0027ca1ed01d
Aladdin <SirAnxiousALot@gmail.com>
2023-05-01 03:02:37 +0200

    Time: 40 ms (13.26%), Space: 16.3 MB (8.21%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.py
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.py
@@ -3,8 +3,5 @@ class Solution:
         salary.sort()
         salary.pop(0)
         salary.pop()
-        total = 0
-        for i in salary:
-            total += i
-        total = total / len(salary)
+        total = sum(salary) / len(salary)
         return total
