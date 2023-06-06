f81ff5ba688d8c4cc6e48043ff0d172f243814a5
Aladdin <SirAnxiousALot@gmail.com>
2023-06-06 18:56:16 +0300

    Time: 50 ms (54.99%), Space: 16.4 MB (51.64%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.py
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.py
@@ -7,7 +7,7 @@ class Solution:
         diff = abs(arr2[0] - arr2[1])
         for i in range(2, len(arr2)):
             if(abs(arr2[i-1] - arr2[i]) != diff):
-                print(diff, abs(arr2[i-1] - arr2[i]) )
+                print(diff, abs(arr2[i-1] - arr2[i]))
                 return False
             else:
                 i += 1
