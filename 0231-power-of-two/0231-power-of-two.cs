0c5696ff45e6b8748b7cec55d48f7b6517e8cffa
Aladdin <SirAnxiousALot@gmail.com>
2024-02-19 19:16:05 +0200

    Time: 42 ms (7.45%), Space: 29.1 MB (8.61%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cs
+++ 0231-power-of-two/0231-power-of-two.cs
@@ -1,14 +1,15 @@
 public class Solution {
     public bool IsPowerOfTwo(int n) {
-        int num = 0;
-        for(int i = 0; i<n; i++)
-        {
-            if(Math.Pow(2, num) == n)
-                return true;
-            if(Math.Pow(2, num) > n)
-                return false;
-            num++;
-        }
-        return false;
+        // int num = 0;
+        // for(int i = 0; i<n; i++)
+        // {
+        //     if(Math.Pow(2, num) == n)
+        //         return true;
+        //     if(Math.Pow(2, num) > n)
+        //         return false;
+        //     num++;
+        // }
+        // return false;
+        return n > 0 && (n & (n - 1)) == 0;
     }
 }
